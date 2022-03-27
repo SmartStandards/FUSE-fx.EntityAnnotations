8fb152e362f72c7863d268cb8f9a884be5925101
T. Korn <email@tobiaskorn.de>
2022-03-27 12:21:39 +0200

    added 'FilterableAttribute', 'IdentityProtectedAttribute' and 'SetableAttribute' to qualify individual fields for common domain-level usecases (non-technical)

--- Lib/ContentAttribute.cs
+++ Lib/ContentAttribute.cs
@@ -4,16 +4,100 @@ using System.Linq;
 namespace System.ComponentModel.DataAnnotations {
 
   /// <summary>
-  /// Defines, that a Property contains Business-Content
+  ///  Defines, that a Property contains Business-Content
   /// </summary>
   [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
   public class ContentAttribute : Attribute {
 
     /// <summary>
-    /// Defines, that a Property contains Business-Content
+    ///  Defines, that a Property contains Business-Content
     /// </summary>
-    public ContentAttribute() {
+    /// <param name="specializedConcern">
+    ///   Indicates, if a field belongs to a more specific business domain.
+    ///   This can be used to control the detail-grade (displayed fields) in the UI
+    ///   or when exporting data in an abstract way.
+    /// </param>
+    public ContentAttribute(string specializedConcern = null) {
+      this.SpecializedConcern = specializedConcern;
     }
+
+    /// <summary>
+    ///   Indicates, if a field belongs to a more specific business domain.
+    ///   This can be used to control the detail-grade (displayed fields) in the UI
+    ///   or when exporting data in an abstract way.
+    /// </summary>
+    public string SpecializedConcern { get; }
+
+  }
+
+  /// <summary>
+  ///  Defines, that a Property contains content, which represents a human readable natural
+  ///  key, that should be preferred when refering to the current record (UI/Logging/Tracing).
+  ///  This is a non-technical information, which relates to the domain model (business-level)!
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+  public class IdentityLabelAttribute : Attribute {
+
+    /// <summary>
+    ///  Defines, that a Property contains content, which represents a human readable natural
+    ///  key, that should be preferred when refering to the current record (UI/Logging/Tracing).
+    ///  This is a non-technical information, which relates to the domain model (business-level)!
+    /// </summary>
+    public IdentityLabelAttribute() {
+    }
+
+  }
+
+  /// <summary>
+  ///  Defines whether a field contains content that is commonly used for filtering.
+  ///  This is a non-technical information, which relates to the domain model (business-level)!
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+  public class FilterableAttribute : Attribute {
+
+    /// <summary>
+    ///  Defines whether a field contains content that is commonly used for filtering.
+    ///  This is a non-technical information, which relates to the domain model (business-level)!
+    /// </summary>
+    public FilterableAttribute(Filterability filterability) {
+      this.Filterability = filterability;
+    }
+
+    public Filterability Filterability { get; }
+
+  }
+
+  /// <summary>
+  ///  Defines whether a field contains content that is commonly used for filtering.
+  ///  This is a non-technical information, which relates to the domain model (business-level)!
+  /// </summary>
+  [Flags]
+  public enum Filterability {
+    None = 0,
+    ExactMatch = 1,
+    Substring = 2
+  }
+
+  /// <summary>
+  ///  Defines whether a field contains content that is sematically owned by the current entity,
+  ///  so that it wont make sense to offer usecases like batch/mass-updates for it.
+  ///  Also when creating a clone (using this entity as template) the content of this field 
+  ///  should not be included by default.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+  public class IdentityProtectedAttribute : Attribute {
+
+    /// <summary>
+    ///  Defines whether a field contains content that is sematically owned by the current entity,
+    ///  so that it wont make sense to offer usecases like batch/mass-updates for it.
+    ///  Also when creating a clone (using this entity as template) the content of this field 
+    ///  should not be included by default.
+    /// </summary>
+    public IdentityProtectedAttribute(bool isIdentityProtected) {
+      this.IsIdentityProtected = isIdentityProtected;
+    }
+
+    public bool IsIdentityProtected { get; }
 
   }
 
--- Lib/FixedAfterCreationAttribute.cs
+++ Lib/FixedAfterCreationAttribute.cs
@@ -2,6 +2,7 @@
 
 namespace System.ComponentModel.DataAnnotations {
 
+  [Obsolete("This attribute is Obsolete - please use 'Setable(Setability.OnCreation)' instead!")]
   [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
   public class FixedAfterCreationAttribute : Attribute {
 
@@ -9,5 +10,58 @@ namespace System.ComponentModel.DataAnnotations {
     }
 
   }
+
+  /// <summary>
+  /// Defines, in which cases a field is ment to be set.
+  /// This is a non-technical information, which relates
+  /// to the domain model (business-level)!
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+  public class SetableAttribute : Attribute {
+
+    public SetableAttribute(Setability setability) {
+      this.Setability = setability;
+    }
+
+    public Setability Setability { get; }
+
+  }
+
+  /// <summary>
+  /// Defines, in which cases a field is ment to be set.
+  /// This is a non-technical information, which relates
+  /// to the domain model (business-level)!
+  /// </summary>
+  [Flags]
+  public enum Setability {
+
+    /// <summary> this field should only be set by wellknown technical automatisms </summary>
+    Never = 0,
+
+    /// <summary> this field should be setable when a record is created newly (or on full-import/recovery) </summary>
+    OnCreation = 1,
+
+    /// <summary> this field should be setable during the usecase of an individual edit</summary>
+    OnSingleUpdate = 2,
+
+    /// <summary> this field should be setable during a batch-update for multiple records</summary>
+    OnBatchUpdate = 4,
+
+    /// <summary> represents the logical combination of 'OnSingleUpdate' + 'OnBatchUpdate' </summary>
+    AfterCreation = 6,
+
+    /// <summary> represents the logical combination of 'OnCreation' + 'AfterCreation' </summary>
+    Always = 7
+
+  }
+
+  //public class Foo {
+  //  void KK() {
+
+  //    Setability s = Setability.Always | Setability.Never;
+  //    bool x = s.HasFlag((Setability) 4 );
+
+  //  }
+  //}
 
 }
